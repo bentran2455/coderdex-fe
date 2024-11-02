bd980588b3ed6cfe9511fa9970f386dfbda54bd6
Ben Tran <letscontactben@gmail.com>
2024-11-02 11:47:05 +0700

    Commit 0211

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-app.use("/api/pokemons", appRouter);
+app.use("/api", appRouter);
 
 app.listen(port, hostname, () => {
   console.log(">>> running on port", port);
